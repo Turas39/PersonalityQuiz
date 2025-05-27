e72c0f45a4845a01c85bf99d490802b2ec8bd549
Turas <tomasz.czaplinski@cyfroweszkoly.pl>
2025-05-28 01:40:56 +0200

    aktualizacja - zmiana kolorow dodanie wiecej funkcjonalnosci

--- app/src/main/java/com/example/personalityquiz/MainActivity.kt
+++ app/src/main/java/com/example/personalityquiz/MainActivity.kt
@@ -2,13 +2,19 @@ package com.example.personalityquiz
 
 import android.app.DatePickerDialog
 import android.app.TimePickerDialog
+import android.graphics.Color
 import android.icu.util.Calendar
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.os.SystemClock
+import android.view.View
+import android.widget.AdapterView
 import android.widget.Button
 import android.widget.Chronometer
+import android.widget.Spinner
 import android.widget.TextView
 import android.widget.TimePicker
+import android.widget.ArrayAdapter
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -18,7 +24,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var chronometer: Chronometer
     private lateinit var button_start : Button
     private var running = false
-    private lateinit var text1 : TextView
+    private lateinit var text_timer : TextView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,6 +39,9 @@ class MainActivity : AppCompatActivity() {
 
         val button_date = findViewById<Button>(R.id.button_date)
         val button_time = findViewById<Button>(R.id.button_time)
+
+        button_date.setBackgroundColor(Color.parseColor("#0000CD"))
+        button_time.setBackgroundColor(Color.parseColor("#0000CD"))
 
         button_date.setOnClickListener {
             val calendar = Calendar.getInstance()
@@ -66,6 +75,60 @@ class MainActivity : AppCompatActivity() {
                 running = true
             }
         }
+
+        button_start.setBackgroundColor(Color.parseColor("#78D317"))
+
+        text_timer = findViewById(R.id.text_timer)
+
+        val countDownTimer: CountDownTimer = object : CountDownTimer(600000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                val secondsLeft = millisUntilFinished / 1000 + 1
+                text_timer.text = "Zostało ci $secondsLeft sekund"
+            }
+
+            override fun onFinish() {
+                SummaryActivity()
+            }
+        }
+        countDownTimer.start()
+
+        val spinner = findViewById<Spinner>(R.id.spinner_pozdro)
+        val decyzje = arrayOf("Kieruję się intuicją – czuję, że tak będzie dobrze",
+                                "Analizuję fakty, dane, plusy i minusy",
+                                "Konsultuję się z innymi, zanim zdecyduję",
+                                "Zazwyczaj odkładam decyzje, dopóki nie muszę ich podjąć",
+                                "Działam spontanicznie, bez dłuższego zastanowienia")
+
+        val adapter = ArrayAdapter( this,
+                                    android.R.layout.simple_list_item_1,
+                                    decyzje
+        )
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                val selectedDecyzje = decyzje[position]
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+
+        }
+
+        val button_zakoncz = findViewById<Button>(R.id.button_zakoncz)
+        button_zakoncz.setOnClickListener {
+            SummaryActivity()
+        }
+
+        button_zakoncz.setBackgroundColor(Color.parseColor("#FF0000"))
 
     }
 }
---
+++ app/src/main/java/com/example/personalityquiz/SummaryActivity.kt
@@ -0,0 +1,4 @@
+package com.example.personalityquiz
+
+class SummaryActivity {
+}
